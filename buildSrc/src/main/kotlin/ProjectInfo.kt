c538e9bb737dd5788d6b19be0cac515f1fe4a6a0
langkye <langkye@gmail.com>
2024-09-22 18:36:32 +0800

    Feature: Release 2.7.2

--- buildSrc/src/main/kotlin/ProjectInfo.kt
+++ buildSrc/src/main/kotlin/ProjectInfo.kt
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------- Project info ----------------------------------------------------------
 const val GROUP_ID = "cn.lnkdoc.sdk"
-const val PROJECT_VERSION = "2.7.1"
+const val PROJECT_VERSION = "2.7.2"
 const val DESCRIPTION = """
     Awesome Uia SDK for Java
     Copyright © 2023 lnkdoc All rights reserved.
