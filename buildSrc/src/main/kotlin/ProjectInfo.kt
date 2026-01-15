2f7ced84749e3633fb4e5c86b4a148dd450cde7f
langkye <langkye@gmail.com>
2026-01-15 19:47:44 +0800

    Feature: 2.x-SNAPSHOT

--- buildSrc/src/main/kotlin/ProjectInfo.kt
+++ buildSrc/src/main/kotlin/ProjectInfo.kt
@@ -1,6 +1,6 @@
 // ---------------------------------------------------------- Project info ----------------------------------------------------------
 const val GROUP_ID = "cn.lnkdoc.sdk"
-const val PROJECT_VERSION = "2.7.2"
+const val PROJECT_VERSION = "2.x-SNAPSHOT"
 const val DESCRIPTION = """
     Awesome Uia SDK for Java
     Copyright © 2023 lnkdoc All rights reserved.
